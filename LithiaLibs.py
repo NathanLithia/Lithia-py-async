4272fc7f70df71551160d6c096c293d1073b4b19
Nathan Greenwood <nathangreenwood@live.com.au>
2017-10-04 05:38:22 +1030

    added new API functions

--- LithiaLibs.py
+++ LithiaLibs.py
@@ -1,5 +1,9 @@
 import os
 
+#CLEAR CONSOLE
+clear = lambda: os.system('cls')
+
+#LEGACY COLORS
 def prRed(prt): print("\033[91m{}\033[00m" .format(prt))
 def prGreen(prt): print("\033[92m{}\033[00m" .format(prt))
 def prYellow(prt): print("\033[93m{}\033[00m" .format(prt))
@@ -9,4 +13,14 @@ def prCyan(prt): print("\033[96m{}\033[00m" .format(prt))
 def prLightGray(prt): print("\033[97m{}\033[00m" .format(prt))
 def prBlack(prt): print("\033[98m{}\033[00m" .format(prt))
 
-clear = lambda: os.system('cls')
+#REVISED COLORS
+liRed = "\033[91m"
+liGreen = "\033[92m"
+liYellow = "\033[93m"
+liLightPurple = "\033[94m"
+liPurple = "\033[95m"
+liCyan = "\033[96m"
+liLightGrey = "\033[97m"
+liBlack = "\033[98m"
+liEND = "\033[00m"
+# Usage: print(LithiaLibs.liCyan + "My Text" + LithiaLibs.liEND)
